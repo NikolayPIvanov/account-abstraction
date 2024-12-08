5ad91dd1f29a7b5d120a85e4c56569a4706d0d9b
Nikolay Ivanov <nick.ivanov98@gmail.com>
2024-12-08 08:28:40 +0200

    Adding receive function

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -46,6 +46,8 @@ contract MinimalAccount is IAccount, Ownable {
         i_entryPoint = IEntryPoint(entryPoint);
     }
 
+    receive() external payable {}
+
     /*//////////////////////////////////////////////////////////////
                            EXTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
